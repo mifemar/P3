37440c02326c087a4ed2b4e3dd288d74bdc6e952
Miguel Fernández Martín <mfernandezmarti@uoc.edu>
2024-06-10 23:20:09 +0200

    First iteration report bugs

---
+++ dist/presentacion.09c24910.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    // Función para hacer vibrar el logo
+    const logo = document.getElementById("logo");
+    let isVibrating = false;
+    logo.addEventListener("mouseover", ()=>{
+        if (!isVibrating) {
+            isVibrating = true;
+            startVibration();
+        }
+    });
+    function startVibration() {
+        const amplitude = 5;
+        const duration = 200;
+        let currentPosition = 0;
+        let direction = 1;
+        const moveLogo = ()=>{
+            currentPosition += amplitude * direction;
+            logo.style.transform = `translateX(${currentPosition}px)`;
+            if (Math.abs(currentPosition) >= amplitude) direction *= -1;
+            if (isVibrating) setTimeout(moveLogo, duration);
+            else logo.style.transform = "";
+        };
+        moveLogo();
+    }
+    logo.addEventListener("mouseout", ()=>{
+        isVibrating = false;
+    });
+    // Función para mostrar un mensaje de agradecimiento al enviar el formulario de la newsletter
+    document.getElementById("newsletter-form").addEventListener("submit", (event)=>{
+        //event.preventDefault();
+        alert("\xa1Muchas gracias por suscribirte a nuestra newsletter!");
+        document.getElementById("newsletter-form").submit();
+    });
+});
+
+//# sourceMappingURL=presentacion.09c24910.js.map
--- dist/presentacion.65e3664a.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("logo"),t=!1;e.addEventListener("mouseover",()=>{t||(t=!0,function(){let n=0,s=1,r=()=>{n+=5*s,e.style.transform=`translateX(${n}px)`,Math.abs(n)>=5&&(s*=-1),t?setTimeout(r,200):e.style.transform=""};r()}())}),e.addEventListener("mouseout",()=>{t=!1}),document.getElementById("newsletter-form").addEventListener("submit",e=>{alert("¡Muchas gracias por suscribirte a nuestra newsletter!"),document.getElementById("newsletter-form").submit()})});
-//# sourceMappingURL=presentacion.65e3664a.js.map
